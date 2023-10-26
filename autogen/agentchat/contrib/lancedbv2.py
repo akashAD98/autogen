4b50535e5ca045c2fb66645d7e9e7931ba1c63f1
Akash A Desai <62583018+akashAD98@users.noreply.github.com>
2023-10-26 19:29:59 +0530

    Update lancedbv2.py

--- autogen/agentchat/contrib/lancedbv2.py
+++ autogen/agentchat/contrib/lancedbv2.py
@@ -148,8 +148,7 @@ def create_lancedb_from_dir(
        
     """
 
-    db_path = "/tmp/lancedb"
-
+    db_path = 'tmp/lancdb'
     # def create_lancedb():
     if client is None:
         db = client.connect(db_path)
@@ -215,8 +214,26 @@ def query_vector_db(
     """
 
 
+    # if query_texts:
+    #     vector = [0.1, 0.3]
+
+    db_path = "/tmp/lancedb"
+
+    db = client.connect(db_path)
+
+    data = [
+                        {"vector": [1.1, 1.2], "id": 11, "documents": "This is a test document spark"},
+                        {"vector": [0.2, 1.8], "id": 22, "documents": "This is another test document"},
+                        {"vector": [0.1, 0.3], "id": 3, "documents": "This is a third test document spark"},
+                        {"vector": [0.5, 0.7], "id": 44, "documents": "This is a fourth test document"},
+                        {"vector": [2.1, 1.3], "id": 55, "documents": "This is a fifth test document spark"},
+                        {"vector": [5.1, 8.3], "id": 66, "documents": "This is a sixth test document"},
+                    ]
+
+    db.create_table(collection_name, data,mode='overwrite')
     if query_texts:
         vector = [0.1, 0.3]
+
     db = lancedb.connect(db_path)
     table = db.open_table("my_table")
     query = table.search(vector).where(f"documents LIKE '%{search_string}%'").limit(n_results).to_df()
